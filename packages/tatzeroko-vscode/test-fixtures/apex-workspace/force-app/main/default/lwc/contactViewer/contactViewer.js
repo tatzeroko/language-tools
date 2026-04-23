bb32a342ac852d60adc8d51456aa82b283cb1510
TrymSeb <trym.sebastian.hegrenes@gmail.com>
2026-04-23 10:10:52 +0200

    fix: update test lwc-component

--- packages/tatzeroko-vscode/test-fixtures/apex-workspace/force-app/main/default/lwc/contactViewer/contactViewer.js
+++ packages/tatzeroko-vscode/test-fixtures/apex-workspace/force-app/main/default/lwc/contactViewer/contactViewer.js
@@ -1,8 +1,12 @@
-import search from "@salesforce/apex/ContactController.search";
+//@ts-check
+/** biome-ignore-all assist/source/organizeImports: This file is used for testing and should only be edited in a way that is relevant to the test. */
+
 import { LightningElement } from "lwc";
+
+import search from "@salesforce/apex/ContactController.search";
 
 export default class ContactViewer extends LightningElement {
 	connectedCallback() {
-		search({ query: "test" });
+		search({ query: "Smith" });
 	}
 }
