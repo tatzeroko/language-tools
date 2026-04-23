798310665dddf3e6f945e6c91855a79329f236f8
TrymSeb <20125012+TrymSeb@users.noreply.github.com>
2026-04-23 09:43:57 +0200

    fix: trim apex workspace fixture

---
+++ packages/tatzeroko-vscode/test-fixtures/apex-workspace/force-app/main/default/lwc/contactViewer/contactViewer.js
@@ -0,0 +1,8 @@
+import search from "@salesforce/apex/ContactController.search";
+import { LightningElement } from "lwc";
+
+export default class ContactViewer extends LightningElement {
+	connectedCallback() {
+		search({ query: "test" });
+	}
+}
--- packages/tatzeroko-vscode/test-fixtures/apex-workspace/src/index.ts
+++
@@ -1,7 +0,0 @@
-import search from "@salesforce/apex/ContactController.search";
-
-async function run() {
-	return await search({ query: "test" });
-}
-
-run();
